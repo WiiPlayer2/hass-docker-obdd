76fd41f9cb128748350e03a82af92b72fa49d747
Waldemar Tomme <waldetomme@gmail.com>
2020-08-15 16:49:03 +0200

    Add ObdService

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import paho.mqtt.client as mqtt
 import obd
 import os
+import time
+import threading
 
 from commands import all_commands
 
-MQTT_BROKER = os.environ.get('MQTT_BROKER', '192.168.1.102')
+IGNORE_OBD_CONNECTION = os.environ.get('IGNORE_OBD_CONNECTION', 'True') == 'True'
+MQTT_BROKER = os.environ.get('MQTT_BROKER', '192.168.1.2')
 MQTT_CLIENT_NAME = os.environ.get('MQTT_CLIENT_NAME', 'obd-service')
 HASS_DISCOVERY_PREFIX = os.environ.get('HASS_DISCOVERY_PREFIX', 'homeassistant')
 OBD_WATCH_COMMANDS = [s.strip() for s in os.environ.get('OBD_WATCH_COMMANDS', ', '.join([
@@ -14,16 +17,45 @@ OBD_WATCH_COMMANDS = [s.strip() for s in os.environ.get('OBD_WATCH_COMMANDS', ',
 ])).split(',')]
 NODE_ID = os.environ.get('NODE_ID', 'car')
 
+class ObdService():
+    def loop_start(self, mqtt_client, watch_commands):
+        threading.Thread(target=self._loop, args=(mqtt_client, watch_commands)).run()
+
+    def _loop(self, mqtt_client, watch_commands):
+        conn = None
+        is_connected = False
+        while True:
+            while not is_connected:
+                try:
+                    conn = obd.Async()
+                    is_connected = conn.status() == obd.OBDStatus.CAR_CONNECTED or IGNORE_OBD_CONNECTION
+                except:
+                    pass
+                if not is_connected:
+                    time.sleep(5)
+
+            cmds = [cmd for cmd in watch_commands if cmd.cmd in conn.supported_commands]
+            for cmd in cmds:
+                cmd.register(HASS_DISCOVERY_PREFIX, NODE_ID, client, conn)
+            conn.start()
+
+            while is_connected:
+                time.sleep(5)
+                try:
+                    is_connected = conn.status() == obd.OBDStatus.CAR_CONNECTED or IGNORE_OBD_CONNECTION
+                except:
+                    is_connected = False
+
+
+
 if __name__ == '__main__':
-    conn = obd.Async()
-    cmds = [cmd for cmd in all_commands if cmd.name in OBD_WATCH_COMMANDS and cmd.cmd in conn.supported_commands]
+    watch_commands = [cmd for cmd in all_commands if cmd.name in OBD_WATCH_COMMANDS]
 
     client = mqtt.Client(MQTT_CLIENT_NAME)
     client.username_pw_set('car', 'car')
     client.connect(MQTT_BROKER)
 
-    for cmd in cmds:
-        cmd.register(HASS_DISCOVERY_PREFIX, NODE_ID, client, conn)
-    conn.start()
+    obd_service = ObdService()
+    obd_service.loop_start(client, watch_commands)
 
     client.loop_forever()

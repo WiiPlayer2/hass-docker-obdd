6215b286a604c497f71dde756c9a072c59c9fec3
Waldemar Tomme <waldetomme@gmail.com>
2020-08-22 11:19:29 +0200

    Add some brackets for clarification

--- commands/decoders.py
+++ commands/decoders.py
@@ -34,9 +34,9 @@ def fuel_level(messages):
 def hv_battery_status(messages):
     _log('hv_battery_status', messages)
     d = messages[0].data
-    battery_current = (d[0] * 256 + d[1]) / 100 - 327.68
-    charge_control = d[2] / 2 - 64
-    discharge_control = d[3] / 2 - 64
+    battery_current = ((d[0] * 256 + d[1]) / 100) - 327.68
+    charge_control = (d[2] / 2) - 64
+    discharge_control = (d[3] / 2) - 64
     delta_soc = d[4] / 2
     soc_ign = d[5] / 2
     soc_max = d[6] / 2
@@ -54,10 +54,10 @@ def hv_battery_status(messages):
 def gforce_and_yaw(messages):
     _log('gforce_and_yaw', messages)
     d = messages[0].data
-    lateral_g = d[0] * 50.02 / 255 - 25.11
-    lineal_g = d[1] * 50.02 / 255 - 25.11
+    lateral_g = (d[0] * 50.02 / 255) - 25.11
+    lineal_g = (d[1] * 50.02 / 255) - 25.11
     yaw_rate = d[2] - 128
-    steering_angle = (d[3] * 256 + d[4]) / 10 - 3276.8
+    steering_angle = ((d[3] * 256 + d[4]) / 10) - 3276.8
     return {
         'lateral_g': lateral_g * units.meter / units.second ** 2,
         'lineal_g': lineal_g * units.meter / units.second ** 2,
